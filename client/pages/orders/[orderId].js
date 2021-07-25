1b817f522b1d7ae184ac36e1a51a54cd4baa4ab3
r-wells <r.l.wells@outlook.com>
2021-07-25 14:15:18 -0500

    added orderid route

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,30 @@
-const OrderShow = () => {
-    return <h1>Show Order</h1>
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+    const [timeLeft, setTimeLeft] = useState('');
+
+    useEffect(() => {
+        const findTimeLeft = () => {
+            const msLeft = new Date(order.expiresAt) - new Date();
+            setTimeLeft(Math.round(msLeft / 1000));
+        };
+
+        findTimeLeft();
+        const timerId = setInterval(findTimeLeft, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        }
+    }, []);
+
+    return <h1>Time left to pay: {timeLeft}</h1>;
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+    const { orderId } = context.query;
+    const { data } = await client.get(`/api/orders/${orderId}`);
+
+    return { order: data }
 }
 
 export default OrderShow;
